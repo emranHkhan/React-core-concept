673fe1cd806a2a958a15f3b6dec7787907bb0fed
Emran H Khan <emran.khan2400@gmail.com>
2021-02-23 17:55:40 +0600

    creating component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person hero="rubel" heroine="brie larson" ></Person>
+        <Person hero="mashrafee" heroine="natalie portman"></Person>
+        <p style={{backgroundColor: 'cyan', padding: '10px', textTransform: 'uppercase', color: 'black'}}>hello React</p>
       </header>
+     
     </div>
   );
 }
+
+//It is a component
+function Person(props){
+  return (<div style={{border: '2px solid black', padding:'10px', margin: '10px'}}>  
+     <h1>hello {props.hero}</h1>
+     <h2>hi {props.heroine}</h2>
+  </div>
+  );
+} 
 
 export default App;
